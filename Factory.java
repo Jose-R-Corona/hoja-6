867ac3ff2b4d8fb49bfabcd4e28ca9da2a66e69f
borsegui <dele14345@uvg.edu.gt>
2015-09-03 17:25:35 -0600

    Update Factory.java

--- Factory.java
+++ Factory.java
@@ -1,8 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Universidad del valle de Guatemala
+Jose Corona
+Diego de León
+Estructura de datos HOJA NO.6
+*/
+
 import java.util.AbstractSet;
 import java.util.HashSet;
 import java.util.TreeSet;
@@ -24,3 +26,4 @@ public class Factory {
    }
 }
     
+
