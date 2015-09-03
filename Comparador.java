f4c01ebd99e3dff05632bd011046470427b7bae3
Stardock <josecorona1460@gmail.com>
2015-09-03 16:46:20 -0600

    v5

--- Comparador.java
+++ Comparador.java
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 
+=======
+/**
+ *Universidad del Valle de Guatemela
+ *Jose corona
+ *Diego de León
+ *estructura de Datos
+ */
+>>>>>>> origin/master
 //importamos la clase comparator
 import java.util.Comparator;
 	public class Comparador implements Comparator<programador>{
